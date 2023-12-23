3d4bdd88dad210be341b1120ed987ede78e75bcf
Bruce Merry <bmerry@gmail.com>
2023-12-23 10:39:54 +0200

    Rewrite again to eliminate client from Connector
    The test now has to use Arc<Mutex> just to keep the borrow checker
    happy, but it simplifies the interface.

--- src/lib.rs
+++ src/lib.rs
@@ -7,81 +7,45 @@ use tokio_modbus::slave::{Slave, SlaveContext};
 use tokio_modbus::{Request, Response};
 
 #[async_trait]
-pub trait Connector: SlaveContext + Send + Debug {
+pub trait Connector: Send + Debug {
     type Output: Client;
 
-    async fn get_client(&mut self) -> Result<&mut Self::Output, Error>;
-    fn disconnect(&mut self);
+    async fn connect(&self, slave: Slave) -> Result<Self::Output, Error>;
 }
 
-pub struct SyncConnector<T: Client, F: Fn(Slave) -> Result<T, Error> + Send> {
+pub struct SyncConnector<T: Client, F: Fn(Slave) -> Result<T, Error> + Send + Sync> {
     factory: F,
-    client: Option<T>,
-    slave: Slave,
 }
 
-impl<T: Client, F: Fn(Slave) -> Result<T, Error> + Send> SyncConnector<T, F> {
-    pub fn new(factory: F, slave: Slave) -> Self {
-        Self { factory, client: None, slave }
+impl<T: Client, F: Fn(Slave) -> Result<T, Error> + Send + Sync> SyncConnector<T, F> {
+    pub fn new(factory: F) -> Self {
+        Self { factory }
     }
 }
 
-impl<T: Client, F: Fn(Slave) -> Result<T, Error> + Send> Debug for SyncConnector<T, F> {
+impl<T: Client, F: Fn(Slave) -> Result<T, Error> + Send + Sync> Debug for SyncConnector<T, F> {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(fmt, "SyncConnector<client = {:?}, slave = {:?}>", self.client, self.slave)
-    }
-}
-
-impl<T: Client, F: Fn(Slave) -> Result<T, Error> + Send> SlaveContext for SyncConnector<T, F>
-{
-    fn set_slave(&mut self, slave: Slave) {
-        self.slave = slave;
-        if let Some(client) = &mut self.client {
-            client.set_slave(slave)
-        }
+        write!(fmt, "SyncConnector()")
     }
 }
 
 #[async_trait]
-impl<T: Client, F: Fn(Slave) -> Result<T, Error> + Send> Connector
-    for SyncConnector<T, F>
-{
+impl<T: Client, F: Fn(Slave) -> Result<T, Error> + Send + Sync> Connector for SyncConnector<T, F> {
     type Output = T;
 
-    async fn get_client(&mut self) -> Result<&mut T, Error> {
-        match self.client {
-            None => {
-                let client = (self.factory)(self.slave)?;
-                Ok(self.client.insert(client))
-            },
-            Some(ref mut client) => Ok(client)
-        }
-    }
-
-    fn disconnect(&mut self) {
-        self.client = None;
+    async fn connect(&self, slave: Slave) -> Result<T, Error> {
+        (self.factory)(slave)
     }
 }
 
 #[derive(Debug)]
 pub struct TcpSlaveConnector {
     socket_addr: SocketAddr,
-    client: Option<Context>,
-    slave: Slave,
 }
 
 impl TcpSlaveConnector {
-    pub fn new(socket_addr: SocketAddr, slave: Slave) -> Self {
-        Self { socket_addr, client: None, slave }
-    }
-}
-
-impl SlaveContext for TcpSlaveConnector {
-    fn set_slave(&mut self, slave: Slave) {
-        self.slave = slave;
-        if let Some(ref mut client) = self.client {
-            client.set_slave(slave);
-        }
+    pub fn new(socket_addr: SocketAddr) -> Self {
+        Self { socket_addr }
     }
 }
 
@@ -89,44 +53,44 @@ impl SlaveContext for TcpSlaveConnector {
 impl Connector for TcpSlaveConnector {
     type Output = Context;
 
-    async fn get_client(&mut self) -> Result<&mut Context, Error> {
-        match self.client {
-            None => {
-                let client = tokio_modbus::client::tcp::connect_slave(self.socket_addr, self.slave).await?;
-                Ok(self.client.insert(client))
-            },
-            Some(ref mut client) => Ok(client)
-        }
-    }
-
-    fn disconnect(&mut self) {
-        self.client = None;
+    async fn connect(&self, slave: Slave) -> Result<Context, Error> {
+        tokio_modbus::client::tcp::connect_slave(self.socket_addr, slave).await
     }
 }
 
 #[derive(Debug)]
 pub struct RobustClient<C: Connector> {
     connector: C,
+    client: Option<C::Output>,
+    slave: Slave,
 }
 
 impl<C: Connector> RobustClient<C> {
-    pub fn new(connector: C) -> Self {
-        Self { connector }
+    pub fn new(connector: C, slave: Slave) -> Self {
+        Self {
+            connector,
+            client: None,
+            slave,
+        }
     }
 }
 
-pub fn new_sync<T: Client, F: Fn(Slave) -> Result<T, Error> + Send>(
+pub fn new_sync<T: Client, F: Fn(Slave) -> Result<T, Error> + Send + Sync>(
     factory: F,
     slave: Slave,
 ) -> RobustClient<SyncConnector<T, F>> {
-    RobustClient::new(SyncConnector::new(factory, slave))
+    RobustClient::new(SyncConnector::new(factory), slave)
 }
 
 pub fn new_tcp_slave(socket_addr: SocketAddr, slave: Slave) -> RobustClient<TcpSlaveConnector> {
-    RobustClient::new(TcpSlaveConnector::new(socket_addr, slave))
+    RobustClient::new(TcpSlaveConnector::new(socket_addr), slave)
 }
 
-pub fn new_rtu_slave(device: impl Into<String>, baud_rate: u32, slave: Slave) -> RobustClient<SyncConnector<Context, impl Fn(Slave) -> Result<Context, Error> + Send>> {
+pub fn new_rtu_slave(
+    device: impl Into<String>,
+    baud_rate: u32,
+    slave: Slave,
+) -> RobustClient<SyncConnector<Context, impl Fn(Slave) -> Result<Context, Error> + Send + Sync>> {
     let device = device.into();
     new_sync(
         move |slave| -> Result<Context, Error> {
@@ -143,14 +107,23 @@ pub fn new_rtu_slave(device: impl Into<String>, baud_rate: u32, slave: Slave) ->
 
 impl<C: Connector> SlaveContext for RobustClient<C> {
     fn set_slave(&mut self, slave: Slave) {
-        self.connector.set_slave(slave)
+        self.slave = slave;
+        if let Some(ref mut client) = self.client {
+            client.set_slave(slave)
+        }
     }
 }
 
 #[async_trait]
 impl<C: Connector> Client for RobustClient<C> {
     async fn call(&mut self, req: Request<'_>) -> Result<Response, Error> {
-        let client = self.connector.get_client().await?;
+        let client = match self.client {
+            None => {
+                let c = self.connector.connect(self.slave).await?;
+                self.client.insert(c)
+            }
+            Some(ref mut c) => c,
+        };
         // TODO: need to put the disconnect/retry logic in here
         client.call(req).await
     }
@@ -159,49 +132,53 @@ impl<C: Connector> Client for RobustClient<C> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::sync::{Arc, Mutex};
     use tokio_modbus::prelude::*;
 
     #[derive(Debug)]
     struct DummyConnector<I: Iterator<Item = Result<Response, Error>> + Send + Debug> {
-        data: I,
-        slave: Slave
+        data: Arc<Mutex<I>>,
+    }
+
+    #[derive(Debug)]
+    struct DummyClient<I: Iterator<Item = Result<Response, Error>> + Send + Debug> {
+        data: Arc<Mutex<I>>,
     }
 
     impl<I: Iterator<Item = Result<Response, Error>> + Send + Debug> DummyConnector<I> {
-        fn new(data: I, slave: Slave) -> Self {
-            Self { data, slave }
-        }
-    }
-
-    impl<I: Iterator<Item = Result<Response, Error>> + Send + Debug> SlaveContext for DummyConnector<I> {
-        fn set_slave(&mut self, slave: Slave) {
-            self.slave = slave;
+        fn new(data: I) -> Self {
+            Self {
+                data: Arc::new(Mutex::new(data)),
+            }
         }
     }
 
     #[async_trait]
     impl<I: Iterator<Item = Result<Response, Error>> + Send + Debug> Connector for DummyConnector<I> {
-        type Output = Self;
+        type Output = DummyClient<I>;
 
-        async fn get_client(&mut self) -> Result<&mut Self, Error> {
-            Ok(self)
-        }
-
-        fn disconnect(&mut self) {
+        async fn connect(&self, _slave: Slave) -> Result<DummyClient<I>, Error> {
+            Ok(DummyClient {
+                data: self.data.clone(),
+            })
         }
     }
 
+    impl<I: Iterator<Item = Result<Response, Error>> + Send + Debug> SlaveContext for DummyClient<I> {
+        fn set_slave(&mut self, _slave: Slave) {}
+    }
+
     #[async_trait]
-    impl<I: Iterator<Item = Result<Response, Error>> + Send + Debug> Client for DummyConnector<I> {
+    impl<I: Iterator<Item = Result<Response, Error>> + Send + Debug> Client for DummyClient<I> {
         async fn call(&mut self, _req: Request<'_>) -> Result<Response, Error> {
-            self.data.next().unwrap()
+            self.data.lock().unwrap().next().unwrap()
         }
     }
 
     #[tokio::test]
     async fn test_success() {
         let responses = vec![Ok(Response::ReadHoldingRegisters(vec![123]))].into_iter();
-        let client = RobustClient::new(DummyConnector::new(responses, Slave(1)));
+        let client = RobustClient::new(DummyConnector::new(responses), Slave(1));
         let client = Box::new(client);
         let client = client as Box<dyn Client>;
         let mut client: Context = client.into();
